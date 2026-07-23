56b51789b56eacf292561e754cddd8b07dd9b48a
Philasande Hadebe <philasand2010@gmail.com>
2026-07-24 01:33:10 +0200

    Add Event Person and Calendar classes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
-# from datetime import datetime
-from email.utils import formataddr
+from dataclasses import dataclass
+from datetime import datetime
+
+# from email.utils import formataddr
 
 """
 Add basic functionality
@@ -19,45 +21,77 @@ Event contains:
 """
 
 
-def pick_calendar(default_calendar_id: int) -> int:
-    prompt = (
-        "(W)ork or (P)ersonal account? \n"
-        f"Press Enter to use your Default Calendar ({default_calendar_id}) "
+@dataclass
+class Person:
+    name: str
+    email: str
+
+
+@dataclass
+class Event:
+    id: int
+    title: str
+    start: datetime
+    end: datetime
+    description: str | None
+    creator: Person
+
+    # def pick_calendar(default_calendar_id: int) -> int:
+    #     prompt = (
+    #         "(W)ork or (P)ersonal account? \n"
+    #         f"Press Enter to use your Default Calendar ({default_calendar_id}) "
+    #     )
+    #     calendar_name: str = input(prompt).strip().upper()
+    #     if calendar_name == "W":
+    #         return 1
+    #     elif calendar_name == "P":
+    #         return 2
+    #
+    #     return default_calendar_id
+    #
+    #
+    # def get_title(title: str) -> str:
+    #     prompt = "Title: "
+    #     title = input(prompt)
+    #     return title
+    #
+    #
+    # # print(pick_calendar(default_calendar_id=5))
+    # # print(get_title("first title"))
+    # # 5. Author Allow any string for [Name Surname]
+    # # but impose structure on email@addre.ss.com
+    # def what_author(default_name: str, default_email: str):
+    #     name_prompt = "Enter your name: "
+    #     email_prompt = "Enter your email: "
+    #     author_name = input(name_prompt)
+    #     author_email = input(email_prompt)
+    #
+    #     if not author_name:
+    #         author_name = default_name
+    #
+    #     if not author_email:
+    #         author_email = default_email
+    #
+    #     formatted = formataddr((author_name, author_email))
+    #     return formatted
+    #
+    #
+    # print(what_author(default_name="John Doe", default_email="johndoe@gmail.com"))
+
+
+# Instantiates the classes and runs all code within (runs as script)
+if __name__ == "__main__":
+    creator1 = Person(name="Philasande Hadebe", email="philasand2010@gmail.com")
+
+    event1 = Event(
+        id=1,
+        title="My Birthday",
+        start=datetime(2005, 8, 22),
+        end=datetime(2005, 8, 22),
+        description=None,
+        creator=creator1,
     )
-    calendar_name: str = input(prompt).strip().upper()
-    if calendar_name == "W":
-        return 1
-    elif calendar_name == "P":
-        return 2
-
-    return default_calendar_id
-
-
-def get_title(title: str) -> str:
-    prompt = "Title: "
-    title = input(prompt)
-    return title
-
-
-# print(pick_calendar(default_calendar_id=5))
-# print(get_title("first title"))
-# 5. Author Allow any string for [Name Surname]
-# but impose structure on email@addre.ss.com
-def what_author(default_name: str, default_email: str):
-    name_prompt = "Enter your name: "
-    email_prompt = "Enter your email: "
-    author_name = input(name_prompt)
-    author_email = input(email_prompt)
-
-    if not author_name:
-        author_name = default_name
-
-    if not author_email:
-        author_email = default_email
-
-    formatted = formataddr((author_name, author_email))
-    return formatted
-
-
-# john = what_author(John Doe, )
-print(what_author(default_name="John Doe", default_email="johndoe@gmail.com"))
+    print("***")
+    print(creator1)
+    print("***")
+    print(event1)
