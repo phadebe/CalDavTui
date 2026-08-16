b94f49bc6ef4713a09f1bb3404ce90d24bee7c71
Phila Hadebe <philasand2010@gmail.com>
2026-08-16 05:38:16 +0200

    Refactored get_author function to only return name and email Brought back get_title Clarified type annotations and took suggestions by pyright

--- main.py
+++ main.py
@@ -2,64 +2,29 @@
 
 # from dataclasses import dataclass
 from datetime import datetime
-from models import Event, Person, Calendar
 
-"""
-Add basic functionality
-to ask a user to add an
-event to their calendar
-Event contains:
-    0. CalendarId
-    1. Title
-    2. EventId
-    3. Start Date
-    4. End Date
-    5. Author
-    6. Desciption
-    7. Colour
-"""
+from models import Calendar, Event, Person
 
 
-# def pick_calendar(default_calendar_id: int) -> int:
-#     prompt = (
-#         "(W)ork or (P)ersonal account? \n"
-#         f"Press Enter to use your Default Calendar ({default_calendar_id}) "
-#     )
-#     calendar_name: str = input(prompt).strip().upper()
-#     if calendar_name == "W":
-#         return 1
-#     elif calendar_name == "P":
-#         return 2
-#
-#     return default_calendar_id
-#
-#
-# def get_title(title: str) -> str:
-#     prompt = "Title: "
-#     title = input(prompt)
-#     return title
-#
-#
-# # print(pick_calendar(default_calendar_id=5))
-# # print(get_title("first title"))
-# # 5. Author Allow any string for [Name Surname]
-# # but impose structure on email@addre.ss.com
-# def what_author(default_name: str, default_email: str):
-#     name_prompt = "Enter your name: "
-#     email_prompt = "Enter your email: "
-#     author_name = input(name_prompt)
-#     author_email = input(email_prompt)
-#
-#     if not author_name:
-#         author_name = default_name
-#
-#     if not author_email:
-#         author_email = default_email
-#
-#     formatted = formataddr((author_name, author_email))
-#     return formatted
-#
-##
+def get_title() -> str:
+    prompt: str = "Title: "
+    title: str = input(prompt)
+    return title
+
+
+# what_author currently returns the building blocks of the author but does not create the object itself
+def get_author() -> tuple[str, str]:
+    name_prompt = "Enter your name: "
+    email_prompt = "Enter your email: "
+    author_name = input(name_prompt)
+    author_email = input(email_prompt)
+
+    return author_name, author_email
+
+
+def get_events(events: list[Event], calendar_id: int) -> list[Event]:
+    return [event for event in events if event.calendar_id == calendar_id]
+
 
 # Instantiates the classes and runs all code within (runs as script)
 if __name__ == "__main__":
@@ -109,36 +74,29 @@ if __name__ == "__main__":
         ),
     ]
 
-    def get_events(events: list[Event], calendar_id: int) -> list[Event]:
-        matching_events: list[Event] = []
-        for event in events:
-            if event.calendar_id == calendar_id:
-                matching_events.append(event.title)
-
-        return matching_events
-
-    print("*1*Type of events object**************************************")
+    print("Type of events object**************************************")
     print("****************************************")
     print("****************************************")
-    print(type(events))
-    print("****************************************")
-    print("****************************************")
-    print("*2*Type of get_events object**************************************")
-    print(type(get_events))
-    print("****************************************")
-    print("****************************************")
-    print("*1*Events in calendar 1**************************************")
-    print("****************************************")
-    print("****************************************")
+    # print("Type of get_events object**************************************")
+    # print(type(get_events))
+    # print("****************************************")
+    # print("****************************************")
+    # print("Events in calendar 1**************************************")
+    # print("****************************************")
+    # print("****************************************")
     print(get_events(events, 1))
-    print("*2*Events in calendar 2**************************************")
     print("****************************************")
     print("****************************************")
+    # print("Events in calendar 2**************************************")
+    # print("****************************************")
+    # print("****************************************")
     print(get_events(events, 2))
-    print("*3*Events in calendar 3**************************************")
+    # print("Events in calendar 3**************************************")
     print("****************************************")
     print("****************************************")
-    print(get_events(events, 3))
-    print("*4*EOL**************************************")
-    print("***************************************")
-    print("***************************************")
+    print(type(get_author()))
+    print("****************************************")
+    print("****************************************")
+    # print("EOL**************************************")
+    # print("***************************************")
+    # print("***************************************")
