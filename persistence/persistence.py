ff020befae5cbe4f767416ceaaeec3fa2b106afa
Phila Hadebe <philasand2010@gmail.com>
2026-09-23 02:58:21 +0200

    Implemented event instances persistence

--- persistence/persistence.py
+++ persistence/persistence.py
@@ -1,6 +1,16 @@
-def save_events():
-    pass
+import json
+import main
+from pathlib import Path
 
 
-def load_events():
-    pass
+path = Path("events.json")
+
+evenzi = main.build_event(1)
+
+def json_to_file(data: str, path) -> None:
+    with open(path, "w", encoding="utf-8") as file:
+        json.dump(evenzi, file)
+        return
+
+def save_event() -> None:
+    
