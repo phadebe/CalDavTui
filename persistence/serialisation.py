caa0f1c9ebcb35821e59d6ea754ea3360de6f55e
Phila Hadebe <philasand2010@gmail.com>
2026-09-23 02:59:45 +0200

    Start work on deserialisation

--- persistence/serialisation.py
+++ persistence/serialisation.py
@@ -1,5 +1,13 @@
+import json
+from dataclasses import asdict
 from datetime import datetime
 from uuid import UUID
+
+import testing
+
+from ..models import Calendar, Event, Person
+
+# Serialisation of Objects to JSON
 
 
 def serialise_default(o):
@@ -9,3 +17,19 @@ def serialise_default(o):
         return str(o)
 
     raise TypeError(f"Object of type {type(o).__name__} is not JSON serializable")
+
+
+def event_to_json(event: Event) -> str:
+    return json.dumps(
+        asdict(event),
+        default=serialise_default,
+    )
+
+
+# Deserialisation of Objects to JSON
+
+
+def json_to_event():
+    with open("events.json") as file:
+        d = json.load(file)
+        print(d)
